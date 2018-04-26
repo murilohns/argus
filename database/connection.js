4b42b069496c487e367adb74b2835c06db9b6791
Murilo Henrique <murilohns@gmail.com>
2018-04-26 17:43:16 -0300

    feature: show db connection error

--- database/connection.js
+++ database/connection.js
@@ -6,6 +6,10 @@ mongoose.Promise = Promise;
 
 let url = process.env.DB_URL;
 
+mongoose.connection.on('error', () => {
+  console.log('mongoose ' + chalk.red('Error connecting to mongo'));
+})
+
 mongoose.connection.once('open', () => {
   console.log('mongoose ' + chalk.green('connected'));
 });
