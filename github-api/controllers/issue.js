830d08988e021cc078164cd35fc0401a32a0d87b
Murilo Henrique <murilohns@gmail.com>
2018-06-01 17:55:17 -0300

    feature: assign issue to supporter

--- github-api/controllers/issue.js
+++ github-api/controllers/issue.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+
 const {
   Issue,
   Repository,
   User,
-  Comment
+  Comment,
+  Supporter
 } = require('../database/models');
+
+const octokit = require('@octokit/rest')();
+octokit.authenticate({
+  type: 'token',
+  token: process.env.GITHUB_API_TOKEN
+});
+
+const slack = require('../helpers/slack');
 
 const find = async query => {
   query = query !== undefined? query : {};
@@ -45,8 +56,68 @@ const update = async (query, update) => {
   return issue;
 };
 
+const assign = async (params) => {
+  const { number } = params.issue;
+  const { action } = params;
+  const { full_name } = params.repository;
+
+  if (action != 'opened') {
+    return {
+      message: 'Already assigned issue',
+      status: 400
+    };
+  }
+
+  const owner = full_name.split('/')[0];
+  const repo = full_name.split('/')[1];
+  const nextAssignSupporter = await Supporter.findOne({}).sort('-last_assign_date');
+
+  const assignees = [nextAssignSupporter.user];
+
+  try {
+    octokit.issues.addAssigneesToIssue({owner, repo, number, assignees})
+      .then((result) => {
+        const issue = result.data;
+        const slackData = {
+          channel: nextAssignSupporter.slack_id,
+          issue: issue,
+          text: `Parabéns, essa <${issue.html_url}|issue> é sua!`,
+          attachments: [
+            {
+              text: issue.body,
+              title: issue.title,
+              title_link: issue.html_url,
+              author_name: issue.user.login,
+              author_icon: issue.user.avatar_url,
+              color: '#43FF2F',
+              fields: [
+                {
+                  title: 'Assignees',
+                  value: nextAssignSupporter.user,
+                  short: false
+                }
+              ]
+            }
+          ]
+        };
+
+        slack.sendMessage(slackData)
+          .then( res => console.log(res))
+          .catch( err => console.log(err));
+      });
+
+    return {
+      message: `Assigned Issue to ${nextAssignSupporter}`,
+      status: 200
+    };
+  } catch (err) {
+    console.log(JSON.stringify(err));
+  }
+};
+
 module.exports = {
   find,
   findOne,
-  update
+  update,
+  assign
 };
